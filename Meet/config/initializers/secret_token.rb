1a0f319a97be2567a0dcdfcee3d628087ff22329
Tedc0819 <tedc890819@gmail.com>
2011-08-30 22:26:35 +0800

    dummy

---
+++ Meet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meet::Application.config.secret_token = 'ff345ce50aaea367ab5610df387b5422a2be46d1af4a1d2a4a11c0a2b5f93c9a5c4c8bd8f4e0646b8408cba2435a3520fdaa4ea2721c7c65190474e4742ef86f'
